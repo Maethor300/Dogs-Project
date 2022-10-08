93faa24b99b7a347d6112219d0d6cc43fac9a473
David Mendoza <davidleomendoza300@gmail.com>
2022-10-08 14:17:37 -0500

    Update 0.0.2

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,14 +1,14 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
       primaryKey :true,
       allowNull:false,
-      autoIncrement: true,
+      defaultValue:Sequelize.UUIDV4,
     },
     name: {
       type: DataTypes.STRING,
--- api/src/routes/gettingName.js
+++ api/src/routes/gettingName.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const { Dog } = require("../db.js")
 const {YOUR_API_KEY} =process.env
-router.get("/",   (req,res)=>{
-  axios.get(`https://api.thedogapi.com/v1/breeds?key=${YOUR_API_KEY}`)
-  .then(response => res.json(response.data))
-});
+router.get("/", (req, res, next)=> {
+   return Dog.findAll()
+   .then(dogs =>{
+     res.send(dogs)
+   });
+})
+// router.get("/",   (req,res)=>{
+//   axios.get(`https://api.thedogapi.com/v1/breeds?key=${YOUR_API_KEY}`)
+//   .then(response => res.json(response.data))
+// });
 router.get("/search", async (req,res)=> {
     let {name} = req.query;
     
@@ -24,5 +31,20 @@ router.get("/search", async (req,res)=> {
        res.status(404).json({error: e.toString()})
     }
 })
+router.post("/", async (req,res)=> {
+  try { 
+  const {name, height, weight, yearsOfLife} = req.body
+  const newDog = await Dog.create({
+    name,
+    height,
+    weight,
+    yearsOfLife,
+  })
+  res.status(200).json(newDog);
+}catch(error){
+  res.status(404).json({error: error.toString()})
+}
+})
+ 
  
 module.exports = router;
