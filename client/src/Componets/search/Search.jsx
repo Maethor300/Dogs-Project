1d71e73d217986b04b5837e51f3bbc43ca4caf9b
David Mendoza <davidleomendoza300@gmail.com>
2022-10-11 09:57:51 -0500

    Update 0.1.2

---
+++ client/src/Componets/search/Search.jsx
@@ -0,0 +1,11 @@
+import "./search.css";
+import react, { Component } from "react";
+import { Link } from "react-router-dom";
+const Search = () => {
+    return (
+      <div className="back-name">
+        <h1>Buscar</h1>
+      </div>
+    )
+}    
+export default Search;
---
+++ client/src/Componets/start/Start.jsx
@@ -0,0 +1,16 @@
+import "./start.css";
+import react, { Component } from "react";
+import IMG1 from "../../img/perros.jpg"
+import { Link } from "react-router-dom";
+const Inicio = () => {
+    return (
+      <div className="back-name">
+        <h1 className="palabra">Henry's Dogs</h1>
+        <Link to= "Home">
+        <img className="App-header" src={IMG1} alt="" />
+        <button className="button-74" role="button">Home</button>
+        </Link>
+      </div>
+    )
+}    
+export default Inicio;
